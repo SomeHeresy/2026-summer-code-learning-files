220f3699e6c5b67d87dc61032cb440397ba411f4
Junzhe Yang <calvin.yang.jz@gmail.com>
2026-06-17 14:22:04 -0700

    added loops.c and edited comments in variable.c

--- C-programmings/Variable.c
+++ C-programmings/Variable.c
@@ -7,6 +7,7 @@ int main(){
 
     //%d for int, %f for float/double, %.2f for 2 decimal places, %c for char, %s for string
     //for printf with variables, dont forget to add those after \n
+    //and \n means next printf message will be printed in a new line
     printf("I'm %s, gonna be %d years old by the end of 2026 summer, right now is like %.2f years old.\n", name, age, acAge);
     
     return 0;
---
+++ C-programmings/loops.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    //loop in java is like: for(int i=0; i<5; i++), C is similar
+    for(int i=0; i<=6; i++){
+        printf("%d ", i);
+    }
+    //so this will print 0 to 6, with one space in between
+
+    return  0;
+}
