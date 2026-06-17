e6e76cd6cd7df7c688a74cdcba24bb912ed2a4a9
Junzhe Yang <calvin.yang.jz@gmail.com>
2026-06-17 14:16:03 -0700

    Edited comments
    add some more notes

--- C-programmings/Variable.c
+++ C-programmings/Variable.c
@@ -5,9 +5,9 @@ int main(){
     double acAge = 18.8;
     char name[] = "Calvin";  // to make char a string must add "[]" after the name of the variable
 
+    //%d for int, %f for float/double, %.2f for 2 decimal places, %c for char, %s for string
     //for printf with variables, dont forget to add those after \n
     printf("I'm %s, gonna be %d years old by the end of 2026 summer, right now is like %.2f years old.\n", name, age, acAge);
-    //the %.2f forces the decimal place to be 2, vice versa
     
     return 0;
 }
