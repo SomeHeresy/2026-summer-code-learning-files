09407c418f9640d5fe747af9a17596390475e2e1
Junzhe Yang <calvin.yang.jz@gmail.com>
2026-06-17 14:30:58 -0700

    added 2 more loop types
    added while and do-while loops

--- C-programmings/loops.c
+++ C-programmings/loops.c
@@ -8,5 +8,23 @@ int main(){
     }
     //so this will print 0 to 6, with one space in between
 
+     printf("\n"); //to make the next printf message in a new line
+
+    //a while loop that prints 67 to 60 :D
+    int ex1 = 67;
+    while(ex1 >= 60){
+        printf("%d ", ex1);
+        ex1--;
+    }
+    
+    printf("\n");
+
+    //a do-while loop that runs at least once, even if the condition is false, but doesnt end if without the ex2--;
+    int ex2 = 69;
+    do{
+        printf("%d ", ex2);
+        ex2--;
+    } while(ex2 >= 67);
+
     return  0;
 }
